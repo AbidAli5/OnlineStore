705337e427a450233821d10d916969dd5cd778bc
AbidAli5 <abidson08@gmail.com>
2023-10-05 15:25:39 +0500

    RoutingSet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ContactUSComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductsListComponent },
+  {path:'Contact Us', component:ContactUSComponent},
+  {path:'About Us', component:AboutUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ShopComponent } from './shop/shop.component';
+import { ContactUSComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    NavBarComponent,
+    ShopComponent,
+    ContactUSComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
